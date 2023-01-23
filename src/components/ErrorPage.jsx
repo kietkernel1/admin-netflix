f27489b73f3855131a6b1a40f2a148f908ac9573
kietkernel1 <kietkernel1@gmail.com>
2023-01-23 15:41:36 +0700

    first commit

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link, useRouteError } from 'react-router-dom'
 import "../scss/errorPage.scss"
 const ErrorPage = () => {
+  const error = useRouteError()
   return (
-    <div className='errorPage-container'>
-        ErrorPage
+    <div>
+        <h1>Oops! Something went wrong</h1>
+        <p>
+          <strong>
+            Error: {error.statusText} {error.status}
+          </strong>
+        </p>
+        <Link to={"/"}>
+        Go to Admin Page
+        </Link>
     </div>
   )
 }
